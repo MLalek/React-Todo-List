c53d3f4441637aa32001402f126a5d06d0ff541a
MLalek <mlalek@gmail.com>
2019-12-18 22:36:04 +0100

    Updates

---
+++ precache-manifest.f094afb46ee0a9fc677acc315c242793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfda97d416dd4f3d09bada6d6023c31f",
+    "url": "/MLalek/React-Todo-List/index.html"
+  },
+  {
+    "revision": "897d063210c41a5e91dd",
+    "url": "/MLalek/React-Todo-List/static/css/main.98b537f2.chunk.css"
+  },
+  {
+    "revision": "c1ea1c53b44f4f36966f",
+    "url": "/MLalek/React-Todo-List/static/js/2.fb6efc47.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MLalek/React-Todo-List/static/js/2.fb6efc47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "897d063210c41a5e91dd",
+    "url": "/MLalek/React-Todo-List/static/js/main.cfd6acc3.chunk.js"
+  },
+  {
+    "revision": "9f6a797b847eba16c60c",
+    "url": "/MLalek/React-Todo-List/static/js/runtime-main.f103adb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MLalek/React-Todo-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
